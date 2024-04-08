25f72dc6545ee6d3122f3b0dc70a0c0118318a14
Amanda Greyce de Sales <109035337+mandyysales@users.noreply.github.com>
2024-04-08 10:51:44 -0300

    Add files via upload

---
+++ ExploraStream.py
@@ -0,0 +1,25 @@
+import io
+import picamera 
+from flask import                                                                                              Flask, Response
+                                                                                                                                                                                                             
+app = Flask(__name__)
+
+def generate_frames():
+	with picamera.PiCamera() as camera:
+		camera.resolution = (640, 480)
+		camera.framerate = 24
+		stream = io.BytesIO()
+		
+		for _ in camera.capture_continuos(stream, 'jpeg', use_video_port = True):
+			stream.seek(0)
+			yield b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + stream.read() + b'|r|n'
+			stream.seek(0)
+			stream.trucate()
+                                        
+@app.routel('/vido_feed')
+def video_feed():                                                                                                      
+	return Response(genarate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__= '__main__':
+	app.run(host = '0.0.0.0', port=5000, threaded=True)
+    
